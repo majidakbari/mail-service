dd508bb5e381b530a5140439c19c8c1056a425bd
Majid Akbari <majid.akbari@neocard.fi>
2019-11-12 12:07:09 +0200

    adding horizon

--- .docker-compose/app/entrypoint.sh
+++ .docker-compose/app/entrypoint.sh
@@ -2,5 +2,7 @@
 
 echo "Running..."
 
+/usr/bin/supervisord -c /etc/supervisor/supervisord.conf
+
 source /etc/apache2/envvars && \
     apache2 -DFOREGROUND
---
+++ app/.bash_history
@@ -0,0 +1,6 @@
+composer require laravel/horizon
+php artisan horizon:install
+php artisan queue:failed-table
+php artisan migrate
+php artisan horizon
+exit
